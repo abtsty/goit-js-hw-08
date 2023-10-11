3460967a0423281199f856c070fb0b6264ea725b
abtsty <andrey.barylov@gmail.com>
2023-10-11 18:42:50 +0300

    thirt task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+// console.dir(form);
+
+let onDataForm = {};
+
+const onElForm = () => {
+  const savedMsg = localStorage.getItem('feedback-form-state');
+  onDataForm = JSON.parse(savedMsg) ?? {};
+  const formDataKeys = Object.keys(onDataForm);
+
+  if (savedMsg) {
+    formDataKeys.map(key => {
+      form.elements[key].value = onDataForm[key];
+    });
+  }
+};
+
+const onInputForm = evt => {
+  onDataForm[evt.target.name] = evt.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(onDataForm));
+};
+
+const onSubmitForm = evt => {
+  evt.preventDefault();
+  localStorage.removeItem('feedback-form-state');
+  console.log(onDataForm);
+
+  evt.target.reset();
+};
+
+onElForm();
+form.addEventListener('input', throttle(onInputForm, 500));
+form.addEventListener('submit', onSubmitForm);
